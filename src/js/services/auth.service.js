5e35505ad4f9c1a3f5c5c651527ac5ce6ae470a2
Konstantin Khomenko <kostya5599@gmail.com>
2020-01-18 22:28:36 +0200

    updated axios & added interceptors

--- src/js/services/auth.service.js
+++ src/js/services/auth.service.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-import API_ENV from '../config/api.config';
+import axios from "../plugins/axios";
 
 /**
  * Function login. Make login request to API
@@ -8,15 +7,11 @@ import API_ENV from '../config/api.config';
  */
 export async function login(email, password) {
     try {
-        const response = await axios.post(`${API_ENV.apiUrl}/auth/login`,
+        const response = await axios.post(`/auth/login`,
             JSON.stringify({ email, password }),
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            },
         );
-        return response.data;
+        console.log(response);
+        return response;
     } catch (err) {
         return Promise.reject(err);
     }
